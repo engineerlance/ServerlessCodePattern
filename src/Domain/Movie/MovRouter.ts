fbfdef4c24480528ddb88f917535431d1d019738
RawadHaber <97979496+RawadHaber@users.noreply.github.com>
2022-05-23 12:22:45 +0100

    cleanup

--- src/Domain/Movie/MovRouter.ts
+++ src/Domain/Movie/MovRouter.ts
@@ -1,19 +1,22 @@
 import { APIGatewayProxyEvent } from "aws-lambda"
+import { InvalidPayload } from "src/Error/ErrorClasses"
 import { createMov, readMov, removeMov } from "./MovService"
 import { movieSchema, paramsValidator, TMovie } from "./MovValidator"
 
 export const router = async (route: APIGatewayProxyEvent["resource"], payload: APIGatewayProxyEvent) => {
-    const body: TMovie = JSON.parse(payload.body!)
-    await paramsValidator(body, movieSchema)
     switch (route) {
         case "/mov":
+            if (!payload.body) throw new InvalidPayload()
+            const body: TMovie = JSON.parse(payload.body)
+            await paramsValidator(body, movieSchema)
             return await createMov(body)
 
         case "/mov/{MovTitle}": {
-            if (payload.httpMethod === "GET") {
-                return await readMov(payload.pathParameters?.MovTitle!)
-            } else {
-                return await removeMov(payload.pathParameters?.MovTitle!)
+            if (payload.httpMethod === "GET" && payload.pathParameters?.MovTitle) {
+                return await readMov(payload.pathParameters.MovTitle!)
+            }
+            if (payload.httpMethod === "DELETE" && payload.pathParameters?.MovTitle) {
+                return await readMov(payload.pathParameters.MovTitle!)
             }
         }
 
